45cbfd803338f818fbc54e6eaa0c73af35fa24f8
AleksandraKaminska <aleksandra-kaminska@outlook.com>
2021-06-13 20:20:51 +0200

    change local index layout

--- ClientApp/src/components/Local/Index.tsx
+++ ClientApp/src/components/Local/Index.tsx
@@ -3,6 +3,7 @@ import { LOCALS_API_URL } from '../../constants';
 import { FiTrash2, FiEdit2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import './Local.css';
+import {NavLink} from "reactstrap";
 
 type Local = {
     id: number
@@ -50,40 +51,43 @@ const List: React.FC<{}> = () => {
 
     return loading ? <p>Loading...</p> : (
       <div>
-        <h1>All locals</h1>
-        <table className="table table-hover table-striped mt-5">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Street</th>
-              <th scope="col">Apartment number</th>
-              <th scope="col">City</th>
-              <th scope="col">Zip code</th>
-              <th scope="col">Nr of tables</th>
-              <th scope="col" />
-            </tr>
-          </thead>
-          <tbody>
-            {locals.map((local, index) => (
-              <tr key={local.id}>
-                <th scope="row">{index + 1}</th>
-                <td>{local.address.street}</td>
-                <td>{local.address.apartmentNumber}</td>
-                <td>{local.address.city}</td>
-                <td>{local.address.zipCode}</td>
-                <td>{local.nrOfTables}</td>
-                <td>
-                  <button className="btn btn-outline-danger" onClick={() => handleRemove(local.id)}>
-                    <FiTrash2 />
-                  </button>
-                  <Link to={`/locals/${local.id}/edit`} className="btn btn-outline-primary ml-2">
-                    <FiEdit2 />
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+          <div className={'header'}>
+              <h1>All locals</h1>
+              <NavLink tag={Link} className="btn btn-primary" to="/locals/new">Add new local</NavLink>
+          </div>
+          <table className="table table-hover table-striped mt-5">
+            <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Street</th>
+                  <th scope="col">Apartment number</th>
+                  <th scope="col">City</th>
+                  <th scope="col">Zip code</th>
+                  <th scope="col">Nr of tables</th>
+                  <th scope="col" />
+                </tr>
+            </thead>
+            <tbody>
+                {locals.map((local, index) => (
+                  <tr key={local.id}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{local.address.street}</td>
+                    <td>{local.address.apartmentNumber}</td>
+                    <td>{local.address.city}</td>
+                    <td>{local.address.zipCode}</td>
+                    <td>{local.nrOfTables}</td>
+                    <td>
+                      <button className="btn btn-outline-danger" onClick={() => handleRemove(local.id)}>
+                        <FiTrash2 />
+                      </button>
+                      <Link to={`/locals/${local.id}/edit`} className="btn btn-outline-primary ml-2">
+                        <FiEdit2 />
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
       </div>
     )
 }
